91e9de0b12908b950a6e93328836fc9a2b52f194
KIM DOEUN <lenter85@twinny.co.kr>
2017-05-03 23:05:59 +0900

    웹팩으로 main.js를 컴파일 했는데 렘브라우저 작동이 잘 된다.  렘은 렌더러 프로세스에서 돌아가고 있고.

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+/**
+ * Created by KIM DOEUN on 2017-04-30.
+ */
+
+'use strict';
+const
+    webpack = require('webpack'),
+    loader = __dirname + '/webpack.loader',
+    path = require('path');
+const APP_PATH = path.join(__dirname, 'js/source');
+const APP_SRC_PATH = path.join(__dirname, 'js/source');
+const PKG = require(path.join(__dirname, 'package.json'));
+const MODULES = PKG.dependencies;
+const EXCLUDED_SRC = /(__tests__|node_modules)/;
+
+var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
+
+
+
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function(x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+var config = {
+    cache: false,
+    context: APP_PATH,
+    entry: {
+        App :__dirname + '/main2.js'
+    },
+
+    output: {
+        path: __dirname,
+        pathinfo: true,
+        filename: 'main.js'
+    },
+    node: {
+        global: true,
+        __dirname: false,
+        __filename: false
+    },
+    target: 'electron',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: EXCLUDED_SRC,
+                include: [
+                    APP_SRC_PATH
+                ],
+                loader: loader
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                query: {
+                    presets: [
+                        'es2015',
+                        "react"
+                    ],
+                    plugins : [
+                        //"transform-remove-console"
+                    ]
+                }
+            }
+        ]
+    },
+    externals: nodeModules,
+    plugins: [
+        new webpack.IgnorePlugin(/\.(css|html|json|md|txt)$/),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
+};
+
+
+module.exports = config
---
+++ webpack.loader.js
@@ -0,0 +1,8 @@
+/**
+ * Created by KIM DOEUN on 2017-04-30.
+ */
+
+'use strict';
+module.exports = function (source) {
+    return source;
+};
